6ba740fac2888eb2946716622e3ed8dd1259258a
BabyDevLight <trishaquismundo408@gmail.com>
2025-01-30 23:11:40 +0800

    Draft: 11:11 PM

---
+++ src/CocoSalesCalculator.java
@@ -0,0 +1,13 @@
+public class CocoSalesCalculator {
+
+
+    public void CocoSalesCalculator(){
+        String phoneName;
+        double phonePrice;
+        int quantitySold;
+        String serviceName;
+        double feePerHour;
+        double serviceHours;
+
+    }
+}
